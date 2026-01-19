3cb00e1d3b005b0d9cc74497f7f6cb5b3bbcb971
John Cannon <92427288+JCannon92@users.noreply.github.com>
2026-01-19 14:44:39 +1030

    Add styling for all elements

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const library = [];
-
 function Book(
     name,
     author,
@@ -14,6 +12,7 @@ function Book(
 }
 
 function addBookToLibrary(
+    library,
     name,
     author,
     genre,
@@ -29,3 +28,45 @@ function addBookToLibrary(
     library.push(newBook);
 }
 
+function displayBook(
+    book,
+) {
+    //Create the text elements
+    const bookCard = document.createElement('div.book-card');
+    const name = document.createElement('p.book-name');
+    const author = document.createElement('p.book-author');
+    const genre = document.createElement('p.book-genre');
+    const pages = document.createElement('p.book-pages');
+
+    //Set their text
+    name.textContent = book.name;
+    author.textContent = book.author;
+    genre.textContent = book.genre;
+    pages.textContent = book.pages;
+
+    // Append to the container
+    bookCard.appendChild(name);
+    bookCard.appendChild(author);
+    bookCard.appendChild(genre);
+    bookCard.appendChild(pages);
+    
+
+    //Create and append the button
+    const bookRemoveButton = document.createElement('button');
+    bookRemoveButton.textContent = 'Remove Book';
+    bookCard.appendChild(bookRemoveButton);
+
+    //Append the card as a child of the card-container element
+    const library = document.querySelector('div.card-container');
+    library.appendChild(bookCard);
+}
+
+function displayAllBooks(
+    library,
+) {
+    
+    library.forEach(displayBook);
+}
+
+
+const library = [];
